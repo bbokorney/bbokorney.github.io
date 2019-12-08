9a30327f5003329834f96e53577265c4f9e65608
Baker Bokorney <bbokorney@gmail.com>
2019-12-08 12:34:53 -0700

    Update site

---
+++ precache-manifest.4e6d4b3cfe85be1d19f5e33a84e5d163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27de78f1d476fa70fef20aa267f7b2b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70cf6da86156fc65787",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "8ecbea31ec0bb9d13311",
+    "url": "/static/css/main.9ae6da4b.chunk.css"
+  },
+  {
+    "revision": "a70cf6da86156fc65787",
+    "url": "/static/js/2.1b48e928.chunk.js"
+  },
+  {
+    "revision": "8ecbea31ec0bb9d13311",
+    "url": "/static/js/main.3f192abd.chunk.js"
+  },
+  {
+    "revision": "b335075317d8b4004897",
+    "url": "/static/js/runtime-main.208a41e2.js"
+  }
+]);
