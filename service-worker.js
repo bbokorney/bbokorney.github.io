077f451fec4082ab1915cebeb3a1f84c0b24861f
Baker Bokorney <bbokorney@gmail.com>
2019-12-08 12:33:03 -0700

    Update site

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4e0e30603e50aa780e0d1bd34c9dcdc.js"
+  "/precache-manifest.4e6d4b3cfe85be1d19f5e33a84e5d163.js"
 );
 
 self.addEventListener('message', (event) => {
