fdfbede8056db00b7fd6dd63cdd47d324e75e324
Baker Bokorney <bbokorney@gmail.com>
2020-01-27 21:48:03 -0700

    Update site to version 0.1.13

---
+++ precache-manifest.deb8448e28f8049b18b53a10b7cc786e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1547da219735de76f037713cefd24adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66ff11e0298744122e97",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a77d49df6e128d7b6cba",
+    "url": "/static/css/main.95cdac99.chunk.css"
+  },
+  {
+    "revision": "66ff11e0298744122e97",
+    "url": "/static/js/2.d8f01fc0.chunk.js"
+  },
+  {
+    "revision": "a77d49df6e128d7b6cba",
+    "url": "/static/js/main.811648a0.chunk.js"
+  },
+  {
+    "revision": "b335075317d8b4004897",
+    "url": "/static/js/runtime-main.208a41e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.279371abcdb8307726e54ae9485150c3.js"
+  "/precache-manifest.deb8448e28f8049b18b53a10b7cc786e.js"
 );
 
 self.addEventListener('message', (event) => {
